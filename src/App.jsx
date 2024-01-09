0d5eb55112d9b3efc65e30d94fe3ed9819e238d2
sidxc72 <sidxc72@gmail.com>
2024-01-09 18:32:52 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+
+import React from "react"
 import Sidebar from "./components/Global/Sidebar"
 import { Outlet } from "react-router-dom"
 import { Provider } from "react-redux"
@@ -12,10 +13,8 @@ function App() {
 
   return (
     <Provider store={store}>
-    <div className ="flex min-h-screen min-w-screen bg-black text-white">
        <Sidebar />
        <Outlet /> 
-    </div>
     </Provider>
       
   )
